8e3accf1bd0ee2cd19f3ba73625896c47966b73d
RicardoCastroBdo <ricardo.castro@bancodeoccidente.com.co>
2021-12-08 12:45:18 -0500

    :building_construction: adding source directory project, README.md file and configuration files

---
+++ my-store/src/app/recap.ts
@@ -0,0 +1,18 @@
+const username: string = "RicardoArquimedes";
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+sum(1,2);
+
+class Person {
+
+
+    constructor( public age: number, public lastName: string ) {
+
+
+    }
+}
+
+const Richie = new Person(31, "Castro");
